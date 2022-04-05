e099e135566abe65f959ba92050ff3d663e107ca
Youngking0914 <60254939+Youngking0914@users.noreply.github.com>
2022-04-05 23:25:32 +0900

    [Add] DFS
    간단하게 구현한 DFS

---
+++ DFS.swift
@@ -0,0 +1,34 @@
+//
+//  DFS.swift
+//  algo
+//
+//  Created by YoungK on 2022/04/05.
+//
+
+import Foundation
+
+var testGraph: [String : [String]] = [
+    "A" : ["B", "C"],
+    "B" : ["A", "D", "E"],
+    "C" : ["A", "F"],
+    "D" : ["B"],
+    "E" : ["B"],
+    "F" : ["C"],
+]
+
+// 인접 리스트 방식 graph
+func DFS<T>(graph: [T : [T]],  first: T) -> [T] {
+    var visitedQueue: [T] = []
+    var needVisitStack: [T] = [first]
+    
+    while !needVisitStack.isEmpty {
+        let node = needVisitStack.removeLast()
+        if visitedQueue.contains(node) { continue }
+        visitedQueue.append(node)
+        needVisitStack.append(contentsOf: graph[node] ?? [])
+    }
+    
+    return visitedQueue
+}
+
+print(DFS(graph: testGraph, first: "A"))
